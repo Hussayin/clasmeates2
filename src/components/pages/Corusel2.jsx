c1fd817816e6a285c5f058c80fa85f3aa4c1e461
Husayin Bahramov <husayinbahramov@gmial.com>
2025-03-10 23:58:31 +0500

    all is truie

--- src/components/pages/Corusel2.jsx
+++ src/components/pages/Corusel2.jsx
@@ -6,17 +6,12 @@ const images = [
   "https://classmeats-uz.netlify.app/stey-mans/abdulaziz.webp",
   "https://classmeats-uz.netlify.app/stey-mans/abduloh.webp",
   "https://classmeats-uz.netlify.app/stey-mans/anvar.webp",
-  "https://classmeats-uz.netlify.app/stey-mans/asal.webp",
   "https://classmeats-uz.netlify.app/stey-mans/bobamurod.webp",
   "https://classmeats-uz.netlify.app/stey-mans/bobur.webp",
-  "https://classmeats-uz.netlify.app/stey-mans/diyano.webp",
-  "https://classmeats-uz.netlify.app/stey-mans/humoyun.webp",
   "https://classmeats-uz.netlify.app/stey-mans/husan.webp",
   "https://classmeats-uz.netlify.app/stey-mans/malika.webp",
-  "https://classmeats-uz.netlify.app/stey-mans/islom.webp",
-  "https://classmeats-uz.netlify.app/stey-mans/izzatbek.webp",
-  "https://classmeats-uz.netlify.app/stey-mans/laylo.webp",
   "https://classmeats-uz.netlify.app/stey-mans/madina.webp",
+  "https://classmeats-uz.netlify.app/stey-mans/izzatbek.webp",
   "https://classmeats-uz.netlify.app/stey-mans/sarvar.webp",
   "https://classmeats-uz.netlify.app/stey-mans/sarvinoz.webp",
 ];
@@ -32,13 +27,13 @@ const FullscreenCarousel = () => {
   }, []);
 
   return (
-    <div className="w-screen h-[95vh] overflow-hidden mt-[60px] relative">
+    <div className="w-screen h-[90vh] overflow-hidden mt-[67px] relative">
       <AnimatePresence>
         <motion.img
           key={index}
           src={images[index]}
           alt="carousel"
-          className="absolute top-0 left-0 w-full h-full object-cover"
+          className="absolute top-0 left-0 w-full h-[90vh] object-top object-cover"
           initial={{ opacity: 0.1 }}
           animate={{ opacity: 1 }}
           exit={{ opacity: 0 }}
@@ -47,7 +42,7 @@ const FullscreenCarousel = () => {
       </AnimatePresence>
       <a
         href="#classmeates"
-        className=" absolute bottom-[110px] z-[100000] flex justify-center items-center w-[100%] "
+        className=" absolute bottom-[85px] z-[100000] flex justify-center items-center w-[100%] "
       >
         <h1 className=" bg-[#000000ca] text-[20px] px-[25px] py-[10px] rounded-2xl  ">
           Scroll down
--- src/components/pages/Enter.jsx
+++ src/components/pages/Enter.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import MenuAll from "../App/MenuAll";
 import Classmeates from "./Classmeates";
 import FullscreenCarousel from "./Corusel2";
@@ -5,15 +6,43 @@ import { Example } from "./HoverAnime";
 import ImageGallery from "./ImagesBar";
 import Navbar from "./Navbar";
 
+const Loader = () => {
+  return (
+    <div className="fixed top-0 gap-[20px] left-0 w-full h-full flex flex-col items-center justify-center bg-gray-900 text-white text-xl font-bold">
+      <p className=" font-nunito mt-[10px] text-[25px] ">11-A Family</p>
+      <img
+        src="https://classmeats-uz.netlify.app/all/LIFE0898.webp"
+        alt="Loading..."
+        className=" w-[80%] rounded-xl "
+      />
+      <p className=" font-nunito text-[25px] "> Iltimos, kuting...</p>
+    </div>
+  );
+};
+
 const Enter = () => {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 5000); // 3 soniya kutish
+  }, []);
+
   return (
     <div className=" overflow-hidden">
-      <Navbar />
-      <FullscreenCarousel />
-      <Example />
-      <Classmeates />
-      <ImageGallery />
-      <MenuAll />
+      {loading ? (
+        <Loader />
+      ) : (
+        <div className=" overflow-hidden">
+          <Navbar />
+          <FullscreenCarousel />
+          <Example />
+          <Classmeates />
+          <ImageGallery />
+          <MenuAll />
+        </div>
+      )}
     </div>
   );
 };
